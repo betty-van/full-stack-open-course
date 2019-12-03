35710f36a9f3054bb51fa84fed5bca8afd1427c1
betty-van <bvan1412@gmail.com>
2019-12-02 19:31:31 -0800

    Part 1c

--- src/index.js
+++ src/index.js
@@ -1,40 +1,100 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
 
 
-// Using props as parameters to components
-// Component names must be capitalized
-const Hello = (props) => {
+const App = (props) => {
+    const [ counter, setCounter ] = useState(0)
+
+    const setToValue = (value) => setCounter(value)
+
     return (
         <div>
-            <p>Hello {props.name}.</p>
-            <p>You are {props.age} years old.</p>
+            <Display counter={counter} />
+            <Button 
+                onClick={() => setToValue(counter + 1)}
+                text='+'
+            />
+            <Button 
+                onClick={() => setToValue(0)}
+                text='zero'
+            />
         </div>
     )
 }
 
-const Footer = () => {
-    return (
-        <div>
-            greeting app created by <a href="https://betty-van.github.io/">Betty Van</a>
-        </div>
-    )
-}
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
 
-// Can also use fragments, wrapping elements to be returned by the component with empty element, this one no longer contains EXTRA div element
-const App = () => {
-    const name = "Peter"
-    const age = 10
+// const App = (props) => {
+//     const {counter} = props
+//     return (
+//         <div>{counter}</div>
+//     )
+// }
 
-    return (
-        <>
-            <h1>Greetings</h1>
-            <Hello name={name} age={age} />
-            <Footer />
-        </>
-    )
-}
+// let counter = 1
+
+// const refresh = () => {
+//     ReactDOM.render(
+//         <App counter={counter} />,
+//         document.getElementById('root')
+//     )
+// }
+
+// setInterval(() => {
+//     refresh()
+//     counter += 1
+// }, 1000)
+
+
+
+
+// // Using props as parameters to components
+// // Component names must be capitalized
+// const Hello = ({name, age}) => {
+//     const bornYear = () => new Date().getFullYear() - age 
+
+//     return (
+//         <div>
+//             <p>Hello {name}.</p>
+//             <p>You are {age} years old.</p>
+//             <p>So you were probably born in {bornYear()}</p>
+//         </div>
+//     )
+// }
+
+// const Footer = () => {
+//     return (
+//         <div>
+//             greeting app created by <a href="https://betty-van.github.io/">Betty Van</a>
+//         </div>
+//     )
+// }
+
+// // Can also use fragments, wrapping elements to be returned by the component with empty element, this one no longer contains EXTRA div element
+// const App = () => {
+//     const name = "Peter"
+//     const age = 10
+
+//     return (
+//         <>
+//             <h1>Greetings</h1>
+//             <Hello name={name} age={age} />
+//             <Hello name="Maya" age={26+10} />
+//             <Footer />
+//         </>
+//     )
+// }
 
 // Can use an array instead of using a root element
 // const App = () => {
@@ -114,4 +174,4 @@ const App = () => {
 //     </div>
 // )
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+// ReactDOM.render(<App/>, document.getElementById('root'))
