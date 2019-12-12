42ff79baf898a6a34cbd4fcecc5014b30fc4d1ca
betty-van <bvan1412@gmail.com>
2019-12-11 20:20:20 -0800

    Finished following along Part e

--- part-2-notes-app/src/App.js
+++ part-2-notes-app/src/App.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 const App = () => {
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState(null)
 
     const notesToShow = showAll
         ? notes
@@ -29,9 +32,12 @@ const App = () => {
                 setNotes(notes.map(note => note.id !== id ? note : returnedNote))
             })
             .catch(error => {
-                alert(
-                    `the note '${note.content}' was already deleted from the server`
+                setErrorMessage (
+                    `Note '${note.content}' was already removed from server`
                 )
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
                 setNotes(notes.filter(n => n.id !== id))
             })
     }
@@ -67,19 +73,21 @@ const App = () => {
     
     return (
         <div>
-        <h1>Notes</h1>
-        <div>
-            <button onClick={() => setShowAll(!showAll)}>
-                show {showAll ? 'important' : 'all'}
-            </button>
-        </div>
-        <ul>
-            {rows()}
-        </ul>
-        <form onSubmit={addNote}>
-            <input value={newNote} onChange={handleNoteChange} />
-            <button type="submit">save</button>
-        </form>
+            <h1>Notes</h1>
+            <Notification message={errorMessage} />
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? 'important' : 'all'}
+                </button>
+            </div>
+            <ul>
+                {rows()}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
+            <Footer />
         </div>
     )
 }
---
+++ part-2-notes-app/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fontSize: 16
+    }
+
+    return (
+        <div style={footerStyle}>
+            <br />
+            <em>Note app, Department of Learning to Program, Betty Van 2019</em>
+        </div>
+    )
+}
+export default Footer
