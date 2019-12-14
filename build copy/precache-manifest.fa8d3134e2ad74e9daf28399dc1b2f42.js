4244cb062bc5ee8e139b7aa1985c48d9b3c08c26
betty-van <bvan1412@gmail.com>
2019-12-14 10:38:18 -0800

    Added front end build

---
+++ build copy/precache-manifest.fa8d3134e2ad74e9daf28399dc1b2f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b98f6b5626260007183b5e8d04eeb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "915df428278ad28b6729",
+    "url": "/static/css/main.38a5f725.chunk.css"
+  },
+  {
+    "revision": "3a9cf1eb61c062aead7f",
+    "url": "/static/js/2.a5cf15af.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.a5cf15af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "915df428278ad28b6729",
+    "url": "/static/js/main.b0e5e075.chunk.js"
+  },
+  {
+    "revision": "82140cb58b9e310928f7",
+    "url": "/static/js/runtime-main.2620c5bc.js"
+  }
+]);
---
+++ build/precache-manifest.2ffc7d8a6d542d78621ea76ba5a388aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b209703f58bac2591b8f3e5cffbf5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23a9627a0ca31982b0b",
+    "url": "/static/css/main.38a5f725.chunk.css"
+  },
+  {
+    "revision": "3a9cf1eb61c062aead7f",
+    "url": "/static/js/2.a5cf15af.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.a5cf15af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f23a9627a0ca31982b0b",
+    "url": "/static/js/main.ab3e059e.chunk.js"
+  },
+  {
+    "revision": "82140cb58b9e310928f7",
+    "url": "/static/js/runtime-main.2620c5bc.js"
+  }
+]);
